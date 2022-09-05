2c376ea9d02e56bd79e0f58dfa5ea24404d47b40
Patricio <patoestradac@gmail.com>
2022-09-05 17:24:37 -0500

    Actividad 5 guardada

---
+++ CompJavaActividad5/src/MainActividad5.java
@@ -0,0 +1,14 @@
+public class MainActividad5 {
+    static int x = 5, y= 10;
+
+    public static void main(String[] args)
+    {
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        System.out.println("Suma = " + (x+y));
+        System.out.println("Resta = " + (x-y));
+        System.out.println("Multiplicación = " + (x*y));
+        System.out.println("División = " + (x/y));
+        System.out.println("Módulo = " + (x%y));
+    }
+}
